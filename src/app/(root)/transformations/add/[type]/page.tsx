d07c1ca64b597de261b53a92caf54f559b85c9f5
Mohamed <info4you2013@gmail.com>
2024-04-14 16:19:34 +0100

    implement clerk webhooks 2

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,32 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TranformationForm from "@/components/shared/TranformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
-	return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = async ({
+	params: { type },
+}: SearchParamProps) => {
+	const { userId } = auth();
+	const tranformation = transformationTypes[type];
+
+	if (!userId) redirect("/sign-in");
+
+	const user = await getUserById(userId);
+
+	return (
+		<>
+			<Header title={tranformation.title} subtitle={tranformation.subTitle} />
+			<TranformationForm
+				action="Add"
+				userId={user._id}
+				type={tranformation.type as TransformationTypeKey}
+				creditBalance={user.creditBalance}
+			/>
+		</>
+	);
 };
 
 export default AddTransformationTypePage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/api/webhooks/clerk"],
+	publicRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
-	matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+// export const config = {
+// 	matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
